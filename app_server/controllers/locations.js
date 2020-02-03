185f5cb3d2251c74475f085f09ff89acaf7bffa8
captdread <jamesbrownre1@gmail.com>
2020-02-03 01:30:15 -0500

    Edited Achitecture to seperate controller routes

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/*GET 'home' page */
+module.exports.homelist = function(req, res) {
+    res.render('index', {title: 'Home'});
+};
+
+/*GET 'Location Info' page */
+module.exports.locationInfo = function(req,res){
+    res.render('index', { title: 'Location Info'});
+};
+
+/*Get 'Add Review' page */
+module.exports.addReview = function(req, res){
+    res.render('index', { title: 'Add Review'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ctrlhomepage = require('../controllers/main')
-/* GET home page. */
-router.get('/', ctrlhomepage.index);
+var ctrlLocations = require('../controllers/locations')
+var ctrlOthers = require('../controllers/others')
+//variables for routing
+
+/* GET location pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/locations', ctrlLocations.locationInfo);
+router.get('/locations/review/new', ctrlLocations.addReview);
+
+/* GET Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
